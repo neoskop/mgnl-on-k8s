3430ee6fe86353336384f161b0cc6663bc3f19f3
Arne Diekmann <arne@neoskop.de>
2022-01-12 16:18:23 +0100

    feat(runtime-env): add env variable to disable connection check

--- images/runtime-env/build/bin/run.sh
+++ images/runtime-env/build/bin/run.sh
@@ -6,6 +6,9 @@ if ! [ -d /home/tomcat/light-modules/mtk ] && [ -w /home/tomcat/light-modules ];
     echo "visible: false" > /home/tomcat/light-modules/mtk/templates/pages/basic.yaml
 fi
 
-java -jar /usr/local/bin/docker-entrypoint.jar
+if [ -z "$DISABLE_CONNECTION_CHECK" ]; then
+    java -jar /usr/local/bin/docker-entrypoint.jar
+fi
+
 export JAVA_OPTS="-Dlog4j2.formatMsgNoLookups=true -XX:InitialRAMPercentage=10 -XX:MaxRAMPercentage=80 -XX:MinRAMPercentage=50 -Dmagnolia.repositories.home=/home/tomcat/magnolia_tmp/repositories -Djava.awt.headless=true -Dfile.encoding=UTF-8 $JAVA_OPTS"
 ${CATALINA_HOME}/bin/catalina.sh run
