2b30327b0dada883955eb5780f815b04f412b2f1
Arne Diekmann <diekmann@neoskop.de>
2025-03-23 20:33:00 +0100

    feat(images): move cache et al. to `/home/tomcat/magnolia_tmp`

--- images/runtime-env/build/bin/run.sh
+++ images/runtime-env/build/bin/run.sh
@@ -7,5 +7,5 @@ if ! [ -d /home/tomcat/light-modules/mtk ] && [ -w /home/tomcat/light-modules ];
 fi
 
 java -jar /usr/local/bin/docker-entrypoint.jar
-export JAVA_OPTS="-Dlog4j2.formatMsgNoLookups=true -XX:InitialRAMPercentage=10 -XX:MaxRAMPercentage=80 -XX:MinRAMPercentage=50 -Dmagnolia.repositories.home=/home/tomcat/magnolia_tmp/repositories -Djava.awt.headless=true -Dfile.encoding=UTF-8 $JAVA_OPTS"
+export JAVA_OPTS="-Dlog4j2.formatMsgNoLookups=true -XX:InitialRAMPercentage=10 -XX:MaxRAMPercentage=80 -XX:MinRAMPercentage=50 -Djava.awt.headless=true -Dfile.encoding=UTF-8 $JAVA_OPTS"
 ${CATALINA_HOME}/bin/catalina.sh run
